588f163a8c66fa9a95b9cae820098197cca0ce35
Anna Yurchik <166827550+aayurchik@users.noreply.github.com>
2026-03-09 20:27:43 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,8 +3,29 @@ from src.auth.users import auth_backend, current_active_user, fastapi_users
 from src.auth.schemas import UserCreate, UserRead
 from src.auth.db import User
 from src.links.router import router as links_router
+from contextlib import asynccontextmanager
+from collections.abc import AsyncIterator
+import asyncio
+from src.tasks.cleanup_simple import delete_expired_links_simple
+from src.core.redis import redis_client
 
-app = FastAPI(title="ShortLink")
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+    async def periodic_cleanup():
+        while True:
+            await delete_expired_links_simple()
+            await asyncio.sleep(3600) 
+
+    task = asyncio.create_task(periodic_cleanup())
+    yield
+    task.cancel()
+    try:
+        await task
+    except asyncio.CancelledError:
+        pass
+    await redis_client.close()
+
+app = FastAPI(title="ShortLink", lifespan=lifespan)
 
 # роутеры аутентификации
 app.include_router(
